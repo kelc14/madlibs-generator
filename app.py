afdba4ed2537441bcd404473a1b66e44947bf117
Kelsey Guffanti <kc.breen3@gmail.com>
2023-03-13 23:10:54 -0400

    fixed variable url, working on css

--- app.py
+++ app.py
@@ -1,58 +1,33 @@
 from flask import Flask, request, render_template
-from stories import Story, story1, story2
+from stories import story_data, stories
 
 app = Flask(__name__)
 
-# story_prompt = []
-# story_text = ""
-
-# story_1_prompts = ['food', 'name', 'adjective', 'noun', 'plural_noun'],
-# story_1_text = """It was {food} day at school, and {name} was super {adjective} for lunch. 
-# But when she went outside to eat, a {noun} stole her {plural_noun}!"""
-
-# story_2_prompts = ["place", "noun", "verb", "adjective", "plural_noun"]
-# story_2_text = """Once upon a time in a long-ago {place}, there lived a
-#        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
-
-# story_prompts = {'story1': story_1_prompts, 'story2': story_2_prompts}
-# story_text = {'story1': story_1_text, 'story2': story_2_text}
-
-
-chosen_story = ""
-
 @app.route('/')
 def home_page():
-    return render_template('home.html')
+    return render_template('home.html', stories=stories)
 
 @app.route('/form')
 def form_page():
-    chosen_story = request.args['s']
-    if chosen_story == 'story1':
-        prompts = story1.prompts
-    elif chosen_story =='story2':
-        prompts = story2.prompts
-    return render_template('form.html', prompts=prompts, chosen_story=chosen_story)
+    story_id = int(request.args['story_id'])
+    chosen_story = story_data[story_id]
+    prompts = chosen_story.prompts
+    print('this is the story ID', story_id)
+    return render_template('form.html', prompts=prompts, story_id=story_id )
 
-@app.route('/story1')
-def make_story1():
+@app.route('/story/<story_id>')
+def generate_story(story_id):
+    story_id = int(story_id)
+
     story_dict = {}
-    prompts = story1.prompts
+    prompts = story_data[story_id].prompts
     print('prompts', prompts)
     for prompt in prompts:
         val = request.args[prompt]
         story_dict[prompt] = val
-    story_text = story1.generate(story_dict)
+    story_text = story_data[story_id].generate(story_dict)
     print('text', story_text)
 
     return render_template('story.html', story_text=story_text)
-    # return '<h1>is this working</h1>'
 
-@app.route('/story2')
-def make_story2():
-    story_dict = {}
-    prompts = story2.prompts
-    for prompt in prompts:
-        val = request.args[f"{prompt}"]
-        story_dict[prompt] = val
-    story_text = story2.generate(story_dict)
-    return render_template('story.html', story_text=story_text)
+
--- stories.py
+++ stories.py
@@ -36,15 +36,24 @@ class Story:
 
 
 # Here's a story to get you started
-story2 = Story(
-    ['food', 'name', 'adjective', 'noun', 'plural_noun'],
-"""It was {food} day at school, and {name} was super {adjective} for lunch. 
-But when she went outside to eat, a {noun} stole her {plural_noun}!""")
+
+
 
 story1 = Story(
     ["place", "noun", "verb", "adjective", "plural_noun"],
     """Once upon a time in a long-ago {place}, there lived a
        large {adjective} {noun}. It loved to {verb} {plural_noun}."""
 )
-answers = {'place':'florida', 'verb':'poke', 'noun':'grape', 'adjective':'pink', 'plural_noun':"mountains"}
+story2 = Story(
+    ['food', 'name', 'adjective', 'noun', 'plural_noun'],
+"""It was {food} day at school, and {name} was super {adjective} for lunch. 
+But when she went outside to eat, a {noun} stole her {plural_noun}!""")
+
+story3 = Story(
+    ['number', 'noun', 'adjective', 'activity', 'clothing', 'food', 'noun'],
+"""One very nice morning near the end of summer, my mother woke me up at {number}:00 A.M. and said, "Wake up and smell the {noun}, {adjective} head! Today is your first day of {activity} and you can't be late." I groaned in my bed for twenty seconds, but eventually I got dressed. I wore a {clothing}. She said, "I made you the breakfast of champions, your favorite, {food}! Have a great day, you are a {noun}!" And off I went. """)
+
+story_data = {1: story1, 2: story2, 3: story3}
+stories = {1:'Ancient Times', 2: "Lunch Time", 3: "First Day", 4: "Story4", 5: "Story5", 6: "Story6"}
+
 
